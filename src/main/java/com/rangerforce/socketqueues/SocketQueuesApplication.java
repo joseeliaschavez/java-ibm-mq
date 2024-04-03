ba81f29532f395a18b634383bb6f01500e95dc4e
Jose Chavez <jose.chavez@dialexa.com>
2024-04-03 15:05:14 -0700

    Gradle init

---
+++ src/main/java/com/rangerforce/socketqueues/SocketQueuesApplication.java
@@ -0,0 +1,13 @@
+package com.rangerforce.socketqueues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketQueuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketQueuesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rangerforce/socketqueues/SocketQueuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rangerforce.socketqueues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketQueuesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
